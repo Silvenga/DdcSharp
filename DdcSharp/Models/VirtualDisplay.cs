a77eb59242597f6bbf699c0b297dc3b018843d28
Mark Lopez <m@silvenga.com>
2017-02-22 22:22:12 -0600

    Prepping decoupled API.

---
+++ DdcSharp/Models/VirtualDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using DdcSharp.Native;
+
+namespace DdcSharp.Models
+{
+    public class VirtualDisplay
+    {
+        public bool IsPrimary { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public IList<PhysicalDisplay> PhysicalDisplays { get; set; }
+    }
+
+    public class PhysicalDisplay : IDisposable
+    {
+        public IntPtr Handle { get; private set; }
+
+        public bool SupportsBrightness { get; set; }
+
+        public bool SupportsColorTemperature { get; set; }
+
+        public void Dispose()
+        {
+            NativeApi.DestroyPhysicalMonitors(Handle);
+        }
+    }
+}
